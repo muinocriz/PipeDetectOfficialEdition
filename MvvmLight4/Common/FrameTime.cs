60af47c5be38935f2cfae10d87f67c101a9a007a
超 <@GONGCHAO>
2019-07-08 20:20:15 +0800

    可以通过设置修改回溯的时间长度

---
+++ MvvmLight4/Common/FrameTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmLight4.Common
+{
+    public class FrameTime : ObservableCollection<int>
+    {
+        public FrameTime()
+        {
+            Add(10);
+            Add(15);
+            Add(20);
+        }
+    }
+}
